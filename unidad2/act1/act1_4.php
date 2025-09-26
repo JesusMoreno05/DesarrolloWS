15a7fa3c1533933eefc30ce3ce5f035e2fdfe628
Jesús Moreno Bernal <jmorber2103@cpifpalanturing.es>
2025-09-26 18:16:37 +0200

    subida elementos

---
+++ unidad2/act1/act1_4.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+---
+title: Ejercicio 4: Número PI
+desc: Formateo del numero PI
+tags: [formato, basico]
+---
+*/
+
+$porcentaje=random_int(0,100);
+
+echo "<div> 
+        <h1>Nivel: $porcentaje %</h1> 
+    </div>
+    <meter min="."0"." max="."100"." value=$porcentaje></meter>"
+    
+
+
+?>
---
+++ unidad2/act2/act2_1.php
@@ -0,0 +1,9 @@
+<?php 
+$a=1;
+
+echo ""."2"." == $a";
+echo "$a == false";
+echo "$a == 2";
+echo "--$a == false";
+
+?>
